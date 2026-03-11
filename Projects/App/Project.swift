9f4e1aef5521d73065efca69dc862cfeb8a4ec27
leesam <toyboy2@hanmail.net>
2026-03-11 14:25:47 +0900

    fix: correct Crashlytics run script to use shell variable instead of xargs

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -67,7 +67,7 @@ let project = Project(
             sources: ["Sources/**"],
             resources: ["Resources/**"],
             //            entitlements: .file(path: .relativeToCurrentFile("Sources/gersanghelper.entitlements")),
-            scripts: [.post(script: "find \"${BUILD_DIR%/Build/*}/SourcePackages\" -name run -path \"*/Crashlytics/run\" | head -1 | xargs -I{} \"{}\"",
+            scripts: [.post(script: "CRASHLYTICS_RUN=$(find \"${BUILD_DIR%/Build/*}/SourcePackages\" -name run -path \"*/Crashlytics/run\" | head -1); \"$CRASHLYTICS_RUN\"",
                             name: "Upload dSYM for Crashlytics",
                             inputPaths: ["${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}",
                                          "${DWARF_DSYM_FOLDER_PATH}/${DWARF_DSYM_FILE_NAME}/Contents/Resources/DWARF/${PRODUCT_NAME}",
